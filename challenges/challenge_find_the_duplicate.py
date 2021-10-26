a2d655c7d6c07af8bb725f4ba37051d068ad5c6e
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-10-26 19:03:19 -0300

    Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) <= 1 or type(nums) != list:
+        return False
+    ordened_nums = sorted(nums)
+    for index in range(len(ordened_nums)):
+        if (
+            type(ordened_nums[index]) != int
+            or ordened_nums[index] < 0
+            or index >= len(ordened_nums) - 1
+        ):
+            return False
+        if ordened_nums[index] == ordened_nums[index + 1]:
+            return ordened_nums[index]
+    return False
