ddf5588ed5fecba600c740c9319a163d43a7a770
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-10-26 19:32:10 -0300

    Retirei uma verificacao do req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,5 @@
 def is_palindrome_iterative(word):
-    if word is None or word == '':
+    if word == '':
         return False
     reversed_word = ''
     for index in range((len(word) - 1), -1, -1):
