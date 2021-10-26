c601b34cf205ed58738dd753f66378ab5dd33400
qu4k3r <neves0007@gmail.com>
2021-10-26 17:09:13 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if len(word) <= 1:
+        return True
+
+    if word[low_index] == word[high_index]:
+        word = word[low_index + 1:high_index]
+        if not word:
+            return True
+
+        return is_palindrome_recursive(word, low_index, len(word) - 1)
+
+    return False
