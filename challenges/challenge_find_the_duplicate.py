90d3237ce9e1e48824313834dc9f663cc2d54cb9
Osvaldo Borges <netoo70@gmail.com>
2021-10-26 18:03:42 -0300

    Requisito 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    for first_index, value in enumerate(nums):
+        for second_index, element in enumerate(nums):
+            if not str(element).isdigit() or element < 0:
+                return False
+            if element == value and first_index != second_index:
+                return element
+    return False
