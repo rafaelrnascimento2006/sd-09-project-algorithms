362b442814e9c91e2c8b66e8c950588523ad870d
Khyrki <khyrkimitsunobi@gmail.com>
2021-10-26 17:04:48 -0300

    Add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
+def verify_entries(period):
+    if type(period[0]) != int or type(period[1]) != int:
+        return None
+    return True
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) != int:
+        return None
+    best_time = 0
+    for period in permanence_period:
+        if verify_entries(period) is None:
+            return None
+        if period[0] <= target_time <= period[1]:
+            best_time += 1
+    return best_time
