1051d45d5065c0f7d977cfaca9b87a33c8617760
Layo Kaminski <layokaminski@gmail.com>
2021-10-26 14:45:15 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        high_counter = 0
+        for enter, leave in permanence_period:
+            if enter <= target_time <= leave:
+                high_counter += 1
+    except TypeError:
+        return None
+    return high_counter
