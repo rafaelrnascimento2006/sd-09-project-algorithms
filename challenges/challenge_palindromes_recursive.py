bb3d3f91c432a1751838f36a630887b1952b7d42
Felipe Cardoso <felipedclc@gmail.com>
2021-10-26 17:52:31 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word, low_index="", high_index=""):
+    """Retorna True se for Palindromo e False se não for"""
+    if _is_palindrome_recursive(word) == word:
+        return True
+    return False
+
+
+def _is_palindrome_recursive(word):
+    if word == "":
+        return False
+
+    if len(word) <= 1:
+        return word
+
+    return _is_palindrome_recursive(word[1:]) + word[0]
+
+
+print(is_palindrome_recursive(""))
+# https://www.ti-enxame.com/pt/python/inverter-uma-string-sem-usar-reversed-ou-1/1042409120/
