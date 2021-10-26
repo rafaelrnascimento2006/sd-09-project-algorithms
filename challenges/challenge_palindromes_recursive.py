780d3cb4397854b640849958b06e50a9e4efb605
Khyrki <khyrkimitsunobi@gmail.com>
2021-10-26 17:22:46 -0300

    lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -6,7 +6,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 1:
         return True
     return is_palindrome_recursive(word[1: high_index], 0, -1)
-
-
-
-""" print(is_palindrome_recursive('gg', 0, 2)) """
