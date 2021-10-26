1a10d9a089abe34bdde1f49444614a1ff16f2014
Felipe Cardoso <felipedclc@gmail.com>
2021-10-26 16:18:26 -0300

    Iniciando projeto algoritimos

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """Busca qual horário possui maior quantidade de acessos."""
+    count = 0
+    try:
+        for permanence in permanence_period:
+            if permanence[0] <= target_time and permanence[1] >= target_time:
+                count += 1
+    except TypeError:
+        return None
+    return count
+
+
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(permanence_period, 2))
