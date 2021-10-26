fd699791fec69e5666218502a70e7e191d0daaf0
tiago-yoneda <xyonedax@gmail.com>
2021-10-26 14:16:14 -0300

    Requisito 1 completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    student_counter = 0
+    try:
+        for (start, stop) in permanence_period:
+            if start and stop:
+                if start <= target_time <= stop:
+                    student_counter += 1
+            else:
+                return None
+    except TypeError:
+        return None
+
+    return student_counter
