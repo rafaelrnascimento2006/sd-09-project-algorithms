c6c494e904e1742638899367f932d81d1230739e
Khyrki <khyrkimitsunobi@gmail.com>
2021-10-26 17:09:02 -0300

    Add req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word[low_index] != word[high_index]:
+        return False
+    if len(word) == 1:
+        return True
+    return is_palindrome_recursive(word[1: high_index], 0, -1)
+
