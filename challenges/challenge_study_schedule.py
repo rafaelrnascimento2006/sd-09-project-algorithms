dd6f334b5b4402c98fce7722c8f4be56774f523e
Luciano_Lodi <lrlodi@gmail.com>
2021-10-26 15:48:57 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,20 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    result = 0
+    if not isinstance(target_time, int):
+        return None
+    try:
+        for time in permanence_period:
+            if not isinstance(time[0], int) or not isinstance(time[0], int):
+                return None
+            if time[0] <= target_time <= time[1]:
+                result += 1
+    except TypeError:
+        return None
+    return result
+
+
+# permanence_test = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# target = 2
+
+# print(study_schedule(permanence_test, target))
