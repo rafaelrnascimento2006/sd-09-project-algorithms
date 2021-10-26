a7bd0ffb908e545c447ed45293a2d2ceb0421082
johnealves <johne_af@hotmail.com>
2021-10-26 15:47:09 -0300

    feat study scherdule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,19 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        count = 0
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                count += 1
+    except TypeError:
+        return None
+
+    return count
+
+
+# permanence_periods = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(permanence_periods, None))  # 3
+# permanence_periods = [(4, None), ("0", 4)]
+# print(study_schedule(permanence_periods, 4))  # 3
+# permanence_periods = [(4, None), ("0", 4)]
+# print(study_schedule(permanence_periods, 4))  # 3
