ad3e26155ee18eba089e221abbc26b3518801f37
BernardoFontes <bfontes.1997@gmail.com>
2021-10-26 14:52:41 -0300

    algoritmos

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    list1 = list(first_string)
+    list2 = list(second_string)
+    if len(list1) != len(list2):
+        return False
+    for letra in list1:
+        if letra not in list2:
+            return False
+        else:
+            return True
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+    for entrada, saida in permanence_period:
+        if type(entrada) != int or type(saida) != int:
+            return None
+        if entrada <= target_time and target_time <= saida:
+            counter += 1
+    return counter
