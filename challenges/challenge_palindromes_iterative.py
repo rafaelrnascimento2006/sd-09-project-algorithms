e735111d409fa92c9c0224449cd9ccf323d8dbfa
Osvaldo Borges <netoo70@gmail.com>
2021-10-26 18:23:34 -0300

    Requisito 05

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    new_word = []
+    if len(word) == 0:
+        return False
+    for letter in list(word):
+        new_word.insert(0, letter)
+    if "".join(new_word) == word:
+        return True
+    else:
+        return False
