61a330887b4f07f5d8d1af22531937fc7a4aa172
ggaldino95 <ggaldino95@gmail.com>
2021-10-26 18:20:15 -0300

    [Task 1] Fixing function complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,11 +3,11 @@ def study_schedule(permanence_period, target_time):
         return None
 
     students_count = 0
-    try:
-        for (hour_in, hour_out) in permanence_period:
+    for (hour_in, hour_out) in permanence_period:
+        if hour_in and hour_out:
             if hour_out >= target_time >= hour_in:
                 students_count += 1
-    except TypeError:
-        return None
+        else:
+            return None
 
     return students_count
