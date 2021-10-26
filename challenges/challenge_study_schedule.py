edc057aa7c6a30a274093949144f751408524054
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-10-26 20:24:22 -0300

    req1 ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students = 0
+
+    try:
+        for permanence_hours in permanence_period:
+            if permanence_hours[0] <= target_time <= permanence_hours[1]:
+                students += 1
+    except TypeError:
+        return None
+
+    return students
