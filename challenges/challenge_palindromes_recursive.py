4c31bc1acda2de05ea4574ec699088e690b250cc
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-10-26 15:59:36 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if(word.isalpha()):
+        if(high_index < low_index):
+            return True
+        else:
+            if(word[low_index] == word[high_index]):
+                return is_palindrome_recursive(
+                    word, low_index + 1, high_index - 1
+                )
+            else:
+                return False
+    return False
