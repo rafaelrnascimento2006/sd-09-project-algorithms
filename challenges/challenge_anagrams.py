4bb71abf5c8fd7f0637d376c4c93eb24f4c68f16
Khyrki <khyrkimitsunobi@gmail.com>
2021-10-26 17:10:02 -0300

    Add req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
+def bubbleSort(list):
+    length = len(list)
+    for index in range(length-1):
+        for index2 in range(0, length-index-1):
+            if list[index2] > list[index2 + 1]:
+                list[index2], list[index2 + 1] = list[index2 + 1], list[index2]
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    list1 = list(first_string)
+    list2 = list(second_string)
+    if list1 == list2:
+        return True
+    else:
+        bubbleSort(list1)
+        bubbleSort(list2)
+    if list1 == list2:
+        return True
+    return False
