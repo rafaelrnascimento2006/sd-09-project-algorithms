1fbe9394b00f5f4c84ba3009ae6cfd780b171215
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-26 20:22:33 -0300

    Requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,7 @@
 def is_palindrome_iterative(word):
+    if not word:
+        return False
     for i in range(len(word) // 2):
-        if word[i] != word[len(word) - i - 1]:
+        if word[i] != word[(i+1) * (- 1)]:
             return False
     return True
