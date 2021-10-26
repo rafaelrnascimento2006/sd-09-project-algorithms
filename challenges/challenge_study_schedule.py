d00d37ba943eb875e74a7029466bf83c66e75a1b
Daniela Muller <danielamuller20@gmail.com>
2021-10-26 18:54:18 -0300

    feat: First requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    best_time = []
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                best_time.append(period)
+    except TypeError:
+        return None
+
+    return len(best_time)
