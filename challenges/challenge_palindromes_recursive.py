86b9334f1bf974a2187d8e77bae84b1d9d21ce58
tiago-yoneda <xyonedax@gmail.com>
2021-10-26 14:47:25 -0300

    arrumando requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,14 +3,9 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if len(word) == 1:
         return True
-    if low_index == high_index + 2:
-        if word[high_index] == word[low_index]:
-            return True
-        else:
-            return False
 
-    if word[high_index] == word[low_index]:
-        if low_index + 1 == high_index:
+    if word[low_index] == word[high_index]:
+        if low_index + 1 == high_index or low_index + 2 == high_index:
             return True
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     else:
