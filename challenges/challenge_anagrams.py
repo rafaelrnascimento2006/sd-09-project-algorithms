e45f4183f0cd4acd7066328719e198273bdd0a29
JRGCast <joao.ralph1@gmail.com>
2021-10-26 19:37:18 -0300

    sei la esse anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,8 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    if not (first_string or second_string):
+    if (first_string or second_string) in (None, ''):
         return False
     else:
         return sorted(first_string) == sorted(second_string)
+
+print(is_anagram('', 'a'))
