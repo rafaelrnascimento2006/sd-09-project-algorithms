b0c4feb6551d95ae5692b038f3dcaced1c234217
PedroMCHenrique <pedro.876.ph@gmail.com>
2021-10-26 17:02:17 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    anagram_check = False
+    sum_string_1 = sum(map(ord, first_string))
+    sum_string_2 = sum(map(ord, second_string))
+    if sum_string_1 == sum_string_2:
+        for letter in second_string:
+            if(len(first_string) == len(second_string)):
+                anagram_check = True
+            else:
+                anagram_check = False
+        else:
+            anagram_check = False
+    return anagram_check
