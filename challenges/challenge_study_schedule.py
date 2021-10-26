987e5cecae55b41194572276d75332c098bdb918
Christopher <chris.tryber@gmail.com>
2021-10-26 16:26:02 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    contador = 0
+    for hours in permanence_period:
+        if type(hours[0]) != int or type(hours[1]) != int:
+            return None
+
+        if hours[0] <= target_time <= hours[1]:
+            contador += 1
+
+    return contador
