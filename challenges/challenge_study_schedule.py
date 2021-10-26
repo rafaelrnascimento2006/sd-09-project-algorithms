f02a9689673466d1fe42a728646acfa26103fe62
brenno-calado <brenno.calado@gmail.com>
2021-10-26 17:57:47 -0300

    study_challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """
+    @param permanence_period `tuple` `list`
+    @param target_time `number`
+    iterates over permanence_period tuples and sums if target_time is between tuple numbers
+    Returns hits `number`
+    """
+    hit = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                hit += 1
+    except TypeError:
+        return None
+    return hit
