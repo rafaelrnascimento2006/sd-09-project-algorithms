cd8913a9de32c4d121b36a5567141fd927010a00
tiago-yoneda <xyonedax@gmail.com>
2021-10-26 14:39:56 -0300

    arrumando req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time:
+    if target_time is None:
         return None
     student_counter = 0
-    try:
-        for (start, stop) in permanence_period:
-            if start and stop:
-                if start <= target_time <= stop:
-                    student_counter += 1
-            else:
-                return None
-    except TypeError:
-        return None
-
+    for (start, stop) in permanence_period:
+        if start and stop:
+            if start <= target_time <= stop:
+                student_counter += 1
+        else:
+            return None
     return student_counter
