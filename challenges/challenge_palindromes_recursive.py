67e7a2901e14b1d271b011846a7c1b1564d272dc
johnealves <johne_af@hotmail.com>
2021-10-26 16:22:24 -0300

    feat function palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    try:
+        if low_index == high_index:
+            return True
+        elif (
+            low_index == high_index - 1 and word[low_index] == word[high_index]
+        ):
+            return True
+        elif word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        else:
+            return False
+    except (IndexError, AssertionError):
+        return False
+
+
+# word = "AGUA"
+# print(is_palindrome_recursive(word, 0, len(word) - 1))
