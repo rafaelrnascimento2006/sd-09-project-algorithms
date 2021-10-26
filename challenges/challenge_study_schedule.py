0b0e168d39ff76436461b3985f46d1abec25d7ca
qu4k3r <neves0007@gmail.com>
2021-10-26 15:49:55 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if not target_time and target_time != 0:
+        return None
+
+    COUNTER = 0
+
+    for ACCESS, EXIT in permanence_period:
+
+        if type(ACCESS) != int or type(EXIT) != int:
+            return None
+
+        if ACCESS <= target_time <= EXIT:
+            COUNTER += 1
+
+    return COUNTER
