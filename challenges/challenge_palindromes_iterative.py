24494a04ad5d3cd0c5b6662a10cd2a565d54b982
tiago-yoneda <xyonedax@gmail.com>
2021-10-26 14:20:28 -0300

    Requisito 5 completo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    mid_point = len(word) // 2
+
+    for index in range(mid_point):
+        if word[index] != word[-(index + 1)]:
+            return False
+    return True
