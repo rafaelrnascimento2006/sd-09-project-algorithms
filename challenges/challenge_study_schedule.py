c3949dbc7af2948be033224b7e98181903c69355
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-10-26 15:35:52 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    sum_stay_in_period = 0
+
+    if(type(target_time) != int):
+        return None
+    for student in permanence_period:
+        try:
+            if(target_time >= student[0] and target_time <= student[1]):
+                sum_stay_in_period += 1
+        except TypeError:
+            return None
+    return sum_stay_in_period
