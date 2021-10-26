a291ca39ab235109aa403c27b355a20e678f3023
Khyrki <khyrkimitsunobi@gmail.com>
2021-10-26 17:22:12 -0300

    adiciona novo if no requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word or word[low_index] != word[high_index]:
         return False
+    if word[low_index] == word[high_index] and len(word) == 2:
+        return True 
     if len(word) == 1:
         return True
     return is_palindrome_recursive(word[1: high_index], 0, -1)
+
+
+
+""" print(is_palindrome_recursive('gg', 0, 2)) """
