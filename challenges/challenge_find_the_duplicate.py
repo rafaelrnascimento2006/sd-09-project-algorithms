a9b94c78062672896f198c6bc821f8b2a66dd2e4
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-10-27 11:21:51 -0300

    requisito 4 feito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,17 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) < 2:
+        return False
+    try:
+        return find_duplicate_algorithm(nums)
+    except TypeError:
+        return False
+
+
+def find_duplicate_algorithm(nums):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if nums[i] < 0 or nums[j] < 0:
+                return False
+            if i != j and nums[i] == nums[j]:
+                return nums[i]
+    return False
