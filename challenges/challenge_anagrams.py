989c35b4d20f503001c1b46705217139d01bcfef
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-10-27 09:45:47 -0300

    feat/Requirements 2 (incomplete) and 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    strings_is_anagram = False
+
+    if len(first_string) != len(second_string):
+        return False
+
+    for letter in first_string:
+        if letter in second_string:
+            strings_is_anagram = True
+        else:
+            strings_is_anagram = False
+            break
+
+        # https://stackoverflow.com/questions/1228299/changing-one-character-in-a-string
+        letter_position = second_string.find(letter)
+        second_string = second_string[:letter_position] + '' + second_string[letter_position+1:]
+    return strings_is_anagram
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # word_is_even = len(word) % 2 == 0
+    word_is_empty = len(word) == 0
+
+    if len(word) == 1:
+        return True
+    if word_is_empty:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if word[low_index] == word[high_index] and low_index != high_index and high_index > 0:
+        is_palindrome_recursive(word, low_index+1, high_index-1)
+    print('é palindromo')
+    return True
