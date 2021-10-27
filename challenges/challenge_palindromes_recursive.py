714b686bd3e15ea217152630bc70a735f341384b
Pedro Henrique <pedro@lifelink.com.br>
2021-10-26 21:54:21 -0300

    feat: req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,18 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if low_index != high_index and word[low_index] == word[high_index]:
-        is_palindrome_recursive(word, low_index + 1, high_index - 1)
-        return True
-
+    is_palidrome = True
+    if low_index < len(word) and high_index < len(word):
+        if (low_index != high_index) and (word[low_index] == word[high_index]):
+            if is_palindrome_recursive(word, low_index + 1, high_index - 1):
+                is_palidrome = True
+            else:
+                is_palidrome = False 
+        else:
+            if word[low_index] != word[high_index] or len(word) == 0:
+                is_palidrome = False
+    if (word == ''):
+        is_palidrome = False
+    return is_palidrome
 
 if __name__ == "__main__":
-    print(is_palindrome_recursive('ana'))
+    words = 'hahh'
+    print(is_palindrome_recursive(words, 0, len(words) - 1))
