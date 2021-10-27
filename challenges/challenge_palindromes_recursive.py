7068483e533f7e8c709ee145af635852edcc417d
JRGCast <joao.ralph1@gmail.com>
2021-10-27 09:38:40 -0300

    resolvido voltar true mesmo quando nao era palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,8 +2,8 @@ def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if not(word):
         return False
-    if high_index - low_index <= 0:
-        return True
+    if high_index - low_index < 1: 
+        return word[::-1] == word
     return (is_palindrome_recursive(word, low_index + 1, high_index - 1))
 
-print(is_palindrome_recursive('socos', 0, 5))
+print(is_palindrome_recursive('socobs', 0, 6))
