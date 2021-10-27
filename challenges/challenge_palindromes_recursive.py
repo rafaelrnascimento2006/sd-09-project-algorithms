dc72d708490a4bcedfb6507e973cc6fd1f502348
Lucas Silva <martssilvapro@gmail.com>
2021-10-27 17:39:23 -0300

    requisito 2 feito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if (not len(word)):
+        return False
+
+    if (low_index == high_index or low_index > high_index):
+        return True
+
+    if (word[low_index] != word[high_index]):
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# print(is_palindrome_recursive('OVO', 0, len('OVO') - 1))
