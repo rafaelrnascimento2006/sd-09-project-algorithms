4124061eb4a4152f04bec4eff4a0e5bd67388735
Raquel Picanço <picanco.raquel@gmail.com>
2021-10-27 16:39:18 -0400

    feat: Solve challenge 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
+def recursive_reverse(list):
+    if not list:
+        return list
+    return [list[-1]] + recursive_reverse(list[:len(list)-1])
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    word = list(word)
+    reverse_word = recursive_reverse(word)
+    return word == reverse_word
