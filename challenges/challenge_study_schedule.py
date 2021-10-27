dc26ce6823b36e4a6ccd82815a386dfe006b169c
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-10-27 13:01:34 -0300

    Lower req1 complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,8 @@ def study_schedule(permanence_period, target_time):
     count = 0
     try:
         for student_permanence in permanence_period:
-            if student_permanence[0] <= target_time:
-                if student_permanence[1] >= target_time:
+            if student_permanence[0] <= target_time and \
+                student_permanence[1] >= target_time:
                     count += 1
     except TypeError:
         return None
