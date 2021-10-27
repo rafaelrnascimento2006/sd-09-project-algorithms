bdf3105e097aabc381e6918c669e0be2ba2fa614
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-26 22:41:47 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,10 +1,12 @@
 def is_anagram(first_string, second_string):
-    if not (first_string and second_string) or (len(first_string) != len(second_string)):
+    if (
+        not (first_string and second_string)
+        or (len(first_string) != len(second_string))
+    ):
         return False
-    for i in range(len(first_string) - 1):
-        if not second_string.count(first_string[i]):
+    for i in range(len(first_string)):
+        if not first_string.count(second_string[i]):
+            return False
+        if not second_string.count(first_string[i]) == first_string.count(second_string[i]):
             return False
     return True
-
-# p1, p2 = 'amrora', 'arrmao'
-# print(is_anagram(p1, p2))
