5a762f69b14c62255978252e843dd0e0b6d4a7c8
ggaldino95 <ggaldino95@gmail.com>
2021-10-26 21:16:25 -0300

    [Task 4] Function implemented successfully

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums or len(nums) == 1:
+        return False
+
+    for index_one, number in enumerate(nums):
+        if type(number) == str or number < 0:
+            return False
+
+        for index_two in range(index_one + 1, len(nums)):
+            if nums[index_one] == nums[index_two]:
+                return nums[index_two]
+
+    return False
