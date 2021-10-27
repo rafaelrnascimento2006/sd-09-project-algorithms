668327790744c9d8ca9a8517661fc2dd5a6576b5
ggaldino95 <ggaldino95@gmail.com>
2021-10-26 22:26:11 -0300

    [Task 5] Function implemented successfully

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    first_index = 0
+    last_index = len(word) - 1
+    while last_index > first_index:
+        if word[first_index] != word[last_index]:
+            return False
+        first_index += 1
+        last_index -= 1
+
+    return True
