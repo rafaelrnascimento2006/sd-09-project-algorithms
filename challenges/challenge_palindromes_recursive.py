3040b261f422b605cf2713a67bf75a19b4eaf9fb
carinebecker <caarine.m@gmail.com>
2021-10-27 17:10:25 -0300

    feat: req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if high_index - low_index == 1 or high_index - low_index == 0:
+        return True
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
