1c28238e1dae20daa380020f1b7eef7d0706f69c
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-10-27 12:28:32 -0300

    fix/Requirement2 - Complete

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,16 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    # word_is_even = len(word) % 2 == 0
     word_is_empty = len(word) == 0
+    word_is_a_letter = len(word) == 1
 
-    if len(word) == 1:
+    if word_is_a_letter:
         return True
     if word_is_empty:
         return False
     if word[low_index] != word[high_index]:
         return False
-    if (
-        word[low_index] == word[high_index]) and (
+    if (word[low_index] == word[high_index]) and (
             low_index != high_index) and (high_index > 0):
-        is_palindrome_recursive(word, low_index+1, high_index-1)
-    print('é palindromo')
+        return is_palindrome_recursive(word, low_index+1, high_index-1)
     return True
