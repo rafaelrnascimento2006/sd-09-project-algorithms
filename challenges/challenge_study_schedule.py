b375c3213db46cdf558654203ac285c6f78d6941
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-10-27 17:05:25 -0300

    Req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    pass
+    students = 0
+    for index_one, index_two in permanence_period:
+        try:
+            if index_one <= target_time <= index_two:
+                students += 1
+        except Exception:
+            return None
+    return students
