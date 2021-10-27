495d62a9bfdb9a81653dacda253f1b3975049a0a
Enio Nicael <enionicael@gmail.com>
2021-10-27 17:44:31 -0300

    Feat: req2, check if a word and palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,18 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # INPUT: string, int, int 
+    # OUTPUT: boolean
+
+    # determinar caso base
+    #       CASO 1: se string vazia ou se os caracteres forem diferentes (nao e palidrome)
+    #       CASO 2: se chegar no meio da string e os caracteres forem iguais (e palindrome)
+    # chamar a propria funcao atualizando os indices
+
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    if high_index == (len(word)//2):
+        return True
+    print('\n==========  DEBUG  ==========')
+    print(f'\n  word: {word}')
+    print(f'\n  low-index: {word[low_index]} = high-index: {word[high_index]}')
+    print('\n=============================')
+    return is_palindrome_recursive(word, low_index+1, high_index-1)
