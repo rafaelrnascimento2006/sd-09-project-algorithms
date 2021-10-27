9deef33d00cda6778845149daefc13f991a3762f
Layo Kaminski <layokaminski@gmail.com>
2021-10-27 10:19:08 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,16 @@
+def search_number(nums):
+    ordered_array = sorted(nums)
+    for index in range(len(ordered_array) - 1):
+        if ordered_array[index] == ordered_array[index + 1]:
+            return ordered_array[index]
+    return False
+
+
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if len(nums) < 2:
+        return False
+    for number in nums:
+        if not isinstance(number, int) or number < 0:
+            return False
+    return search_number(nums)
