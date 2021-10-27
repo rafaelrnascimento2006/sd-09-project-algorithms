86b415839e402edd8e7b5e818e4bca11c13e2574
Raquel Picanço <picanco.raquel@gmail.com>
2021-10-27 16:18:09 -0400

    feat: Solve challenge 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if isinstance(target_time, int):
+        try:
+            periods_match = [
+                (start_time, end_time)
+                for start_time, end_time in permanence_period
+                if start_time <= target_time <= end_time
+            ]
+            return len(periods_match)
+        except TypeError:
+            pass
+    return None
