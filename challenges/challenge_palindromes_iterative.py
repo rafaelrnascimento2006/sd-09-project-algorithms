8bb7f9dc8350ea2745be386698b69f906282dc93
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-27 02:31:00 -0300

    melhorando performance ao nao utilizar a desnecessaria lista

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,8 +1,10 @@
 def is_palindrome_iterative(word):
     if len(word) == 0:
         return False
-    substring_list = list(word)
-    for i in range(len(substring_list)-1):
-        if substring_list[i] != substring_list[len((substring_list))-(i+1)]:
+    for i in range(len(word)-1):
+        if word[i] != word[len(word)-(i+1)]:
             return False
     return True
+
+
+print(is_palindrome_iterative('a'))
