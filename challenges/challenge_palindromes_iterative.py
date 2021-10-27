9740d37aaf6222f7e8a2f8579dfc3b6d27e04b93
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-26 23:01:29 -0300

    Requisito 5 teste de juste para reduzir o tempo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,6 +2,6 @@ def is_palindrome_iterative(word):
     if not word:
         return False
     for i in range(len(word) // 2):
-        if word[i] != word[(i+1) * (- 1)]:
+        if word[i] != word[len(word)-i - 1]:
             return False
     return True
