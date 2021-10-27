d273892183d7cabae19916642a707fe3ffb542d7
Enio Nicael <enionicael@gmail.com>
2021-10-27 19:56:02 -0300

    Feat: req 1, write pseudocode

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    print("Starting the project :)")
+    """
+    INPUT:
+        LISTA [
+            TUPLA (2, 2) -> (horario de entrada, horario de saida)
+        ],
+        INT -> horario alvo fornecido como parametro
+
+    OUTPUT: INT -> quantidade de alunos que estavam presentes no periodo
+        fornecido como parametro
+
+    PROCEDURE:
+        inicializar o contador com 0
+        percorrer a lista de tuplas
+            a cada tupla verificar se o TARGET_TIME esta no intervalo de
+            entrada e saida do aluno
+                SE tiver no intervalo SOMAR + 1
+        retornar a quantidade de alunos presentes no TARGET_TIME
+    """
+    
