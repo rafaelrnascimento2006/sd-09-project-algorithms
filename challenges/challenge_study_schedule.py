4daede7518d8782846470521bf3000b5d394680a
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-10-27 12:46:05 -0300

    pamsalesr and The Al Gore Rhytm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    try:
+        for student_permanence in permanence_period:
+            if student_permanence[0] <= target_time and student_permanence[1] >= target_time:
+                count += 1
+    except TypeError:
+        return None
+    return count
