ba6f859d2d9b2cb87bc412be435115254057d121
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-10-27 19:39:23 -0400

    add project

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if low_index == high_index:
+        return True
+    elif word != "" and word[low_index] == word[high_index]:
+        if low_index > high_index:
+            return True
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
+
+
+print(is_palindrome_recursive('', 0, -1))
