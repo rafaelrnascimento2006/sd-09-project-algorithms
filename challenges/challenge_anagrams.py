42f83678ddcf18699d5abd9921078074fc175100
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2021-10-27 20:10:55 -0300

    Req 03 - [WIP]

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    len_first = len(first_string)
+    len_second = len(second_string)
+    if (not first_string or not second_string) and (len_first != len_second):
+        return False
+
+    for letter_first in first_string:
+        for letter_second in second_string:
+            if letter_first == letter_second:
+                first_string = first_string.replace(letter_first, "")
+                second_string = second_string.replace(letter_second, "")
+    if len(first_string) == 0 and len(second_string) == 0:
+        return True
+
+
+# is_anagram('asd', 'sda')
