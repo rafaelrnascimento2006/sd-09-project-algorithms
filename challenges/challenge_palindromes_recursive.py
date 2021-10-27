2bcd711acb0bfa790815f4624d2d3e13d825174b
JRGCast <joao.ralph1@gmail.com>
2021-10-27 09:31:07 -0300

    palindromo recursivo ta tenso

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    # mas o que são esses index? What?
     """ Faça o código aqui. """
+    if not(word):
+        return False
+   
+
+print()
