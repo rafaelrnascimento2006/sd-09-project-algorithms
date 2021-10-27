554149e49db63a30f0791257472c251d15192843
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-10-27 09:26:05 -0300

    feat/Requirement1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    student = None
+    index = 1
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int) or target_time is None:
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            student = index
+            index += 1
+    return student
