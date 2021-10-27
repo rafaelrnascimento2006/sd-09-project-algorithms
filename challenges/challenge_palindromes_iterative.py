21b5a4057262e1dde563d35922cc7d343de39a71
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-27 01:45:22 -0300

    palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    substring_list = list(word)
+    for index in range(len(substring_list)-1):
+        if substring_list[index] != substring_list[len((substring_list))-(index+1)]:
+            return False
+    return True
