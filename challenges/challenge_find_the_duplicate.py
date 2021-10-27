006f7708755e7f8bb21a28dc873615f4f1475c61
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-10-27 13:51:52 -0300

    refatorando codigo

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,8 +1,13 @@
+from challenges.floyds_tortoise_and_hare import floyds_tortoise_and_hare
+
+
 def find_duplicate(nums):
     if len(nums) < 2:
         return False
     try:
-        return find_duplicate_algorithm(nums)
+        if len(nums) == 2 and nums[0] != nums[1]:
+            return False
+        return floyds_tortoise_and_hare(nums)
     except ValueError:
         return False
 
@@ -11,7 +16,7 @@ def find_duplicate_algorithm(nums):
     for i in range(len(nums)):
         if int(nums[i]) < 0:
             return False
-        for j in range(len(nums[slice(i)])):
+        for j in range(len(nums[:i])):
             if nums[i] == nums[j]:
                 return nums[i]
     return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,4 +1,7 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-    return all(word[i] == word[-(i + 1)] for i in range(len(word) // 2))
+    for i in range(len(word) // 2):
+        if word[i] != word[-(i + 1)]:
+            return False
+    return True
