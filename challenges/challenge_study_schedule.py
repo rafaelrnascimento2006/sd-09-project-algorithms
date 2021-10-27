2c82b8d7fbe56e4a1b807eb42bcfea1f396cd0c6
JRGCast <joao.ralph1@gmail.com>
2021-10-26 22:52:18 -0300

    carambolas, perdi muito mais tempo que imaginava nesse study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    matchHorario = 0 # quantas vezes o target time foi encontrado entre o periodo de entrada e saída
+    if target_time is None:
+        return None
+    
+    for [horario1, horario2] in permanence_period:
+        if (horario1 or horario2) is None:
+            return None
+        if horario1 <= target_time <= horario2: # target time precisa estar entre os horários para dar o match
+            matchHorario += 1 # toda vez que der match do target time entre a hora que o aluno entrou e a hora que saiu 
+    return matchHorario
+
+	
+arrTuple = [(80, 80), (80, 80), (80, 90), (1, 5), (4, 5), (4, 5)]
+hor = 80
+
+print(study_schedule(arrTuple, hor))
