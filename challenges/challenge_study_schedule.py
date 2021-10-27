fe2dadb92a656eb9ff5f82f4df32e6a8bcc46ecf
lcgoandete <lcgoandete@hotmail.com>
2021-10-26 21:03:45 -0300

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    counter = 0
+    for entrance, exit in permanence_period:
+        if not entrance or not exit:
+            return None
+
+        if entrance <= target_time <= exit:
+            counter += 1
+    return counter
