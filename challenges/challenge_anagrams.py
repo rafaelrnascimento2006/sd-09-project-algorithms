b2845c08760bf2c8fbbb5cc8c17b945c3b587ae4
brenno-calado <brenno.calado@gmail.com>
2021-10-27 17:11:34 -0300

    is_anagram using min

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,4 +2,16 @@ def is_anagram(first_string, second_string):
     """"""
     if len(first_string) != len(second_string):
         return False
+    first = quicksort(first_string)
+    second = quicksort(second_string)
+    if first != second:
+        return False
     return True
+
+def quicksort(string):
+    if len(string) <= 1:
+        return string
+
+    min_string = string[string.index(min(string))]
+    splitted = string.split(min_string)
+    return (min_string * (len(splitted) - 1)) + quicksort(''.join(splitted))
