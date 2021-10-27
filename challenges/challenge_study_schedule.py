78cd734974e2076fb20d36bd7610c6cdba73c978
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-10-26 21:17:23 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    estudantes = 0
+    for pessoa in permanence_period:
+        if (
+            isinstance(pessoa[0], int)
+            and isinstance(pessoa[1], int)
+            and target_time is not None
+        ):
+            if (
+                pessoa[0] <= target_time <= pessoa[1]
+                and estudantes is not None
+            ):
+                estudantes += 1
+        else:
+            estudantes = None
+    return estudantes
