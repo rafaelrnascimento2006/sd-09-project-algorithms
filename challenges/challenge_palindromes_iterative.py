bea3c01340e7f4c8ab525f53441a4a47170b8c0f
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-27 01:45:56 -0300

    is_palindrome_iterative refactor

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,7 +2,7 @@ def is_palindrome_iterative(word):
     if len(word) == 0:
         return False
     substring_list = list(word)
-    for index in range(len(substring_list)-1):
-        if substring_list[index] != substring_list[len((substring_list))-(index+1)]:
+    for i in range(len(substring_list)-1):
+        if substring_list[i] != substring_list[len((substring_list))-(i+1)]:
             return False
     return True
