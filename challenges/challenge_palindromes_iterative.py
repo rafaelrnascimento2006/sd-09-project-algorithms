28d606b73cfef07688f42b51f1671dc2513fe55c
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-10-26 21:56:39 -0300

    mudanca no req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,8 +1,8 @@
 def is_palindrome_iterative(word):
     if word == '':
         return False
-    mid = (len(word) // 2) if len(word) % 2 == 0 else (len(word) // 2) + 1
+    mid = len(word) // 2
     for index in range(0, mid):
-        if word[index] != word[-abs(index + 1)]:
+        if word[index] != word[-(index + 1)]:
             return False
     return True
