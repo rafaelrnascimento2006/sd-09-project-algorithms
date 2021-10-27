54bb1924c32994810da953d8d64f351dd85274a5
Lucas Silva <martssilvapro@gmail.com>
2021-10-27 16:15:46 -0300

    Requisito 1 feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students = []
+    if target_time is None:
+        return None
+    for begin, end in permanence_period:
+        if (not isinstance(begin, int) or not isinstance(end, int)):
+            return None
+        if (begin <= target_time <= end):
+            students.append((begin, end))
+    return len(students)
