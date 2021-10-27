9abed9fcd9e9a8745467c9d30101f0230146c60a
Raquel Picanço <picanco.raquel@gmail.com>
2021-10-27 18:13:24 -0400

    feat: Solve challenge 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,8 @@
-def find_duplicate(nums):
-    """ Faça o código aqui. """
+def find_duplicate(nums: list) -> list:
+    if isinstance(nums, list):
+        for num in nums:
+            if str(num).isalpha() or num < 0:
+                break
+            elif nums.count(num) > 1:
+                return num
+    return False
