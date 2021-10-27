9259583477eaa6de8bd729f869b0940581f38499
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-10-27 13:05:10 -0300

    Req1 flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,8 +3,8 @@ def study_schedule(permanence_period, target_time):
     try:
         for student_permanence in permanence_period:
             if student_permanence[0] <= target_time and \
-                student_permanence[1] >= target_time:
-                    count += 1
+                    student_permanence[1] >= target_time:
+                count += 1
     except TypeError:
         return None
     return count
