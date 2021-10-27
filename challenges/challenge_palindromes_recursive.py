810c0c288a4d06ffb5dc9948ee3165169f6c3c6c
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-27 00:21:27 -0300

    Palindrome - more refactor...just for  you flakes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if len(word) == 0:
+    if len(word) == 0 or word[low_index] != word[high_index]:
         return False
-    if len(word) == 1:
+    if len(word) == 1 or low_index == high_index:
         return True
-    if low_index == high_index:
-        return True
-    if word[low_index] != word[high_index]:
-        return False
     if low_index == high_index - 1 and word[low_index] == word[high_index - 1]:
         return True
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
