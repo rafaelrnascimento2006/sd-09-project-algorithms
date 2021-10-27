124cb9ce99ae1604d294fb8affd95717d290db3a
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-26 22:39:31 -0300

    Recursive Palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if len(word) == 1:
+        return True
+    if low_index == high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index == high_index - 1 and word[low_index] == word[high_index - 1]:
+        return True
+    low_index += 1
+    high_index -= 1
+    return is_palindrome_recursive(word, low_index, high_index)
