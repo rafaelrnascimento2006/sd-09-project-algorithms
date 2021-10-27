777d55bd78321c0755d419cd09c01783a635f8f8
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-26 22:00:46 -0300

    Anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not (first_string and second_string) or (len(first_string) != len(second_string)):
+        return False
+    for i in range(len(first_string) - 1):
+        if not second_string.count(first_string[i]):
+            return False
+    return True
+
+# p1, p2 = 'amrora', 'arrmao'
+# print(is_anagram(p1, p2))
