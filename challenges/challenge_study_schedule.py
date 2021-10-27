aa8e2f2844912a79742347af22014f6d4ea1916c
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-26 21:04:17 -0300

    req one - study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    if target_time is None:
+        return None
+    for tupla in permanence_period:
+        if tupla[1] is None:
+            return None
+        if tupla[1] >= target_time and tupla[0] <= target_time:
+            count += 1
+    return count
