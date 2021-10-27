9be081794b32daebe5feaa9a60c358758aead319
JRGCast <joao.ralph1@gmail.com>
2021-10-27 09:34:30 -0300

    estranho, == 0 da erro, mas <= 0 da certo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,6 +2,8 @@ def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if not(word):
         return False
-   
+    if high_index - low_index <= 0:
+        return True
+    return (is_palindrome_recursive(word, low_index + 1, high_index - 1))
 
-print()
+print(is_palindrome_recursive('socos', 0, 5))
