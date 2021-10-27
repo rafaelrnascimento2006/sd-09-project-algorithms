5f0c0e58b0d90db2dc014a30b933aa4377977d8f
Christopher <chris.tryber@gmail.com>
2021-10-27 16:49:06 -0300

    Requisito 3, falta acertar o tempo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,24 @@
+def bubble_sort(array):
+    has_swapped = True
+
+    num_of_iterations = 0
+
+    while has_swapped:
+        has_swapped = False
+
+        for i in range(len(array) - num_of_iterations - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                has_swapped = True
+
+        num_of_iterations += 1
+
+    return array
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    first_string_sorted = "".join(bubble_sort(list(first_string)))
+    second_string_sorted = "".join(bubble_sort(list(second_string)))
+    return first_string_sorted == second_string_sorted
