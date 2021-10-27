0eec8e5a2e50318bd6e5583aa9a7df43d94f6df2
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-10-26 22:03:06 -0300

    Req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    elif word[low_index] == word[high_index]:
+        if low_index < high_index - 1:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        else:
+            return True
+    else:
+        return False
