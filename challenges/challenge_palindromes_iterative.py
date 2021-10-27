78675e5adcf18dc99c67805876b5a55d8ef21237
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-27 02:33:29 -0300

    trocando iteraçao do for para uma iteraçao com while ... muito interessante, nao esperava tamanha diferença

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,10 +1,11 @@
 def is_palindrome_iterative(word):
     if len(word) == 0:
         return False
-    for i in range(len(word)-1):
-        if word[i] != word[len(word)-(i+1)]:
+    smaller_index = 0
+    bigger_index = len(word) - 1
+    while smaller_index < bigger_index:
+        if word[smaller_index] != word[bigger_index]:
             return False
+        smaller_index += 1
+        bigger_index -= 1
     return True
-
-
-print(is_palindrome_iterative('a'))
