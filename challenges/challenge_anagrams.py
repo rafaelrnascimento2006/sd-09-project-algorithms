e3ad7b926a5e7865a22cc26e1a128238b6e989e4
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-26 22:45:40 -0300

    Ajustes flake8

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,6 +7,9 @@ def is_anagram(first_string, second_string):
     for i in range(len(first_string)):
         if not first_string.count(second_string[i]):
             return False
-        if not second_string.count(first_string[i]) == first_string.count(second_string[i]):
+        if not (
+            second_string.count(first_string[i]) ==
+            first_string.count(second_string[i])
+        ):
             return False
     return True
