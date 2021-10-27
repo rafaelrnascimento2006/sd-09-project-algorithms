efddc01d0cecbbb2a4c0bca1e641c28de9d20222
JRGCast <joao.ralph1@gmail.com>
2021-10-27 08:41:12 -0300

    bubble sort no is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,5 +3,22 @@ def is_anagram(first_string, second_string):
     if (first_string or second_string) in (None, ''):
         return False
     else:
-        return sorted(first_string) == sorted(second_string)
-     # se não explicitar que o sort() n pode ser usado eu n vejo rsrs
+        fstlist=list(first_string)
+        sndlist=list(second_string)
+        # https://panda.ime.usp.br/panda/static/pythonds_pt/05-OrdenacaoBusca/OBubbleSort.html
+        for each_letter in range(len(fstlist)-1,0,-1):
+            for i in range(each_letter):
+                if fstlist[i]>fstlist[i+1]:
+                    temp = fstlist[i]
+                    fstlist[i] = fstlist[i+1]
+                    fstlist[i+1] = temp
+        for each_letter in range(len(sndlist)-1,0,-1):
+            for i in range(each_letter):
+                if sndlist[i]>sndlist[i+1]:
+                    temp = sndlist[i]
+                    sndlist[i] = sndlist[i+1]
+                    sndlist[i+1] = temp
+
+    return fstlist == sndlist
+
+print(is_anagram('amor', 'roma'))
