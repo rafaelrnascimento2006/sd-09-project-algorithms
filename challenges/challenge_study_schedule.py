ae73cc23282c4854f0e4001581b1555828efbc4f
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-10-27 00:12:26 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    # #vqv
+    number_of_students = 0
+    if type(target_time) != int:
+        return None
+    for (entry, exit) in permanence_period:
+        if type(entry) != int or type(exit) != int:
+            return None
+        if entry <= target_time <= exit:
+            number_of_students += 1
+    return number_of_students
