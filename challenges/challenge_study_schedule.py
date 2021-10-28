a436ac58a7851ad266c2618a286942e48329fdb0
Vagner Pereira <sousa.vagner82@gmail.com>
2021-10-28 11:56:36 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,9 @@
 def study_schedule(permanence_period, target_time):
     repeat_counter = 0
     for item in permanence_period:
-        if type(item[0]) != int or type(item[1]) != int or target_time is None:
+        try:
+            if item[0] <= target_time <= item[1]:
+                repeat_counter += 1
+        except Exception:
             return None
-        if item[0] <= target_time <= item[1]:
-            repeat_counter += 1
     return repeat_counter
