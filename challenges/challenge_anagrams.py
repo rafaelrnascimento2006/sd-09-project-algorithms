402ce70a5677b1ba8c7d939da7178f0d5199a2b5
carinebecker <caarine.m@gmail.com>
2021-10-28 09:47:48 -0300

    fix: req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,26 +1,17 @@
 def is_anagram(first_string, second_string):
-    if not first_string or not second_string:
-        return False
-    
-    first = []
-    second = []
-    
-    for f_element in first_string:
-        first.append(f_element)
+    first = [item for item in first_string]
+    second = [item for item in second_string]
 
-    for s_element in second_string:
-        second.append(s_element)
-    
     for f_element in range(len(first) - 1):
         for i in range(f_element + 1, len(first)):
             if first[f_element] > first[i]:
                 first[f_element], first[i] = first[i], first[f_element]
-    
+
     for s_element in range(len(second) - 1):
         for i in range(s_element + 1, len(second)):
             if second[s_element] > second[i]:
                 second[s_element], second[i] = second[i], second[s_element]
-    
+
     if first == second:
         return True
     return False
