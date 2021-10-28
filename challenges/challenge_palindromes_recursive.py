7596b610a5739379045bbda61434415532d5722b
lcgoandete <lcgoandete@hotmail.com>
2021-10-27 21:14:34 -0300

    requisito 03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,6 +3,5 @@ def is_palindrome_recursive(word, low_index, high_index):
         if low_index >= high_index:
             return True
         elif word[low_index] == word[high_index]:
-            return is_palindrome_recursive(word, low_index +1, high_index -1)
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return False
-    
