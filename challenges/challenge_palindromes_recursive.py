6eafd7abe48402fc98173db8b58200bc1bf2286b
cleber-teixeira <cleberlt@gmail.com>
2021-10-27 23:45:54 -0300

    Req 02 - Palíndromos (Recursividade). :white_check_mark:

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word == '' or word[int(low_index)] != word[int(high_index)]:
+        return False
+    elif high_index == 0:
+        return True
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
