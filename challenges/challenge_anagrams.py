2f2d46f63663f875be3902136593d6f7cb82f1c7
carinebecker <caarine.m@gmail.com>
2021-10-28 18:09:26 -0300

    fix: req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,15 +1,36 @@
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+
+    mid = len(list) // 2
+    left, right = merge_sort(list[:mid]), merge_sort(list[mid:])
+    return merge(left, right, list.copy())
+
+def merge(left, right, list_merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            list_merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            list_merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+            
+    for left_cursor in range(left_cursor, len(left)):
+            list_merged[left_cursor + right_cursor] = left[left_cursor]
+    
+    for right_cursor in range(right_cursor, len(right)):
+            list_merged[left_cursor + right_cursor] = right[right_cursor]
+    return list_merged
+
+
 def is_anagram(first_string, second_string):
     first = [item for item in first_string]
     second = [item for item in second_string]
 
-    if len(first_string) == len(second_string):
-        for f_element in range(len(first_string)):
-            for i in range(f_element + 1, len(first_string)):
-                if first[f_element] > first[i]:
-                    first[f_element], first[i] = first[i], first[f_element]
-                if second[f_element] > second[i]:
-                    second[f_element], second[i] = second[i], second[f_element]
-
-    if first == second:
+    if merge_sort(first) == merge_sort(second):
         return True
     return False
+
+
+print(is_anagram('bolas', 'lobo'))
