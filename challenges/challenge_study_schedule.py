4a31a8d97a985bdf911097ff24a13f36b202dcf4
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-27 22:09:06 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    count_target = 0
+    for start, end in permanence_period:
+        if not(start and end):
+            return None
+        if start <= target_time <= end:
+            count_target += 1
+    return count_target
