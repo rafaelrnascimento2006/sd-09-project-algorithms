7b3d6640ccd50be55808abce9672a9c16f2ebf9f
Julianafatsil <julianafatsil@gmail.com>
2021-10-28 00:02:33 -0300

    Challenge - 02 [Create] Function is_palindrome_recursive with algorithms recursive search

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """1º validações se len(word) == 0 return false
+    2ª verificar o inicio e o fim se são iguais
+    3º se for igual chamar a função novamente
+    passando inicio +1 e final -1
+    - se algum momento não for igual return false
+    """
+    if len(word) == 0:
+        return False
+    if word[low_index] == word[high_index]:
+        if low_index + 1 > high_index - 1:
+            return True
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
