eb5d0db8e1fc27b9e935e4f98261c0f6d28c8b03
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-10-28 19:47:46 -0300

    Feat: ta indo... Vamos que vamos...

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,14 @@
 permanence_period2 = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
 
+
 def study_schedule(permanence_period, target_time):
     sum_period = 0
     try:
         for elemento in permanence_period:
-          if elemento[0] <= target_time <= elemento[1]:
-              sum_period += 1
+            if elemento[0] <= target_time <= elemento[1]:
+                sum_period += 1
         return sum_period
     except TypeError:
         return None
 
 # print(study_schedule(permanence_period2, 3))
-
