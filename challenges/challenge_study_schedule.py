404d4f52088393d2e47832a62157a06e3f42b550
cleber-teixeira <cleberlt@gmail.com>
2021-10-27 21:15:40 -0300

    Req 01 - Número de estudantes estudando no mesmo horário (Algoritmo de busca). :white_check_mark:

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count_students = 0
+    try:
+        for time in permanence_period:
+            if time[0] <= target_time <= time[1]:
+                count_students += 1
+    except TypeError:
+        return None
+
+    return count_students
