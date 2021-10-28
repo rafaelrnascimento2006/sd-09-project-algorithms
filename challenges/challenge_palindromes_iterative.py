dfd89630dbad8c7cac2c2eee99a65c199ec84f6e
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-10-27 21:12:09 -0300

    req 2 refactored and req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not(word):
+        return False
+    low_index, high_index = 0, len(word) - 1
+    while high_index > low_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,7 +3,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     # https://youtu.be/WPSeyjX1-4s?t=1752
     if not(word):
         return False
-    if high_index - low_index < 1:
+    if not(high_index > low_index):
         return True
     return ((word[low_index] == word[high_index]) and (
         is_palindrome_recursive(word, low_index + 1, high_index - 1)))
