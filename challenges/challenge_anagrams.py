f493fc9633678b7d49679c678094cc14b07f23d9
genadireuben <genadireuben91@gmail.com>
2021-10-27 21:10:56 -0300

    challenge anagrams resolvido

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,33 @@
 def is_anagram(first_string, second_string):
-    
+    first_list = list(first_string)
+    second_list = list(second_string)
+    if len(first_string) == len(second_string):
+        sorted_first_list = merge_sort(first_list)
+        sorted_second_list = merge_sort(second_list)
+        if sorted_first_list == sorted_second_list:
+            return True
+    return False
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+    return merged
