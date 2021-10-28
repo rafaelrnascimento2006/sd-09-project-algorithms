c020f13de0ad02f6f9364317932c5723caad2528
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-27 22:49:16 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums:
+        return False
+    for num in nums:
+        if type(num) != int or num < 1:
+            return False
+        if nums.count(num) > 1:
+            return num
+    return False
