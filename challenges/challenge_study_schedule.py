94c8045d42e28ee253837aaf05857e422d84e0f3
Thais Quintela <thais.s.quintela@gmail.com>
2021-10-29 00:59:50 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Iniciando o projeto. """
+    number_of_students_present = 0
+
+    for period in permanence_period:
+        try:
+            if period[0] <= target_time <= period[1]:
+                number_of_students_present += 1
+        except TypeError:
+            return None
+
+    return number_of_students_present
