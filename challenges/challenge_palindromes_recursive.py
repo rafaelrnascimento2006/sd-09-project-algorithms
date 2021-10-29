d1d724f91ad16984ac7546a531dfcf915180baff
edison-ps <edison@osite.com.br>
2021-10-29 12:54:51 -0300

    Requisito 2 V1.01

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,15 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
-    
+
     if (low_index <= high_index and low_index != high_index):
         if (word[low_index] == word[high_index]):
             low_index += 1
             high_index -= 1
             return is_palindrome_recursive(word, low_index, high_index)
-            
+ 
         else:
             return False
-    
+
     if (word != ""):
         return True
     else:
