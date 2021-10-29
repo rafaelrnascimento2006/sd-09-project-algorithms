3c914cb361f70c45957643b6d0607dddb348699a
Vagner Pereira <sousa.vagner82@gmail.com>
2021-10-29 14:52:44 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,11 @@
 def is_anagram(first_string, second_string):
     count = 0
+    second_string_aux = second_string
     for i in first_string:
-        for j in second_string:
+        for j in second_string_aux:
             if i == j:
                 count += 1
+                second_string_aux = second_string_aux.replace(j, '', 1)
                 break
     if count == len(first_string) == len(second_string):
         return True
