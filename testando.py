60d5962e0cf2e659c6dcc69f5c2ac2c2ec1c8e83
Uilton <itoclick@gmail.com>
2021-10-29 15:56:26 -0300

    testando o teste

--- testando.py
+++ testando.py
@@ -36,10 +36,14 @@ teste(permanence_period, 4)
 def is_palindrome_recursive(word, low_index, high_index):
     if word == "":
         return False
-    x = (word[::-1] == word, low_index)
-    if x[1] >= 1:
-        return x[0]
-    return is_palindrome_recursive(word, low_index + 1, high_index)
+
+    if word[::-1] != word:
+        return False
+
+    if word[low_index] == word[high_index]:
+        return True
+
+    return is_palindrome_recursive(word, low_index, high_index)
 
 
 wrd = "yyyyy"
