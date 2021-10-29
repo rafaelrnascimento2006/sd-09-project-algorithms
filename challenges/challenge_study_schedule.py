78a80783b27eb9b98140d382b493eb2ac978a6f4
Bruno Brolesi <brunohsbrolesi@gmail.com>
2021-10-28 22:33:26 -0300

    feat: req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) != int:
+        return None
+
+    counter = 0
+
+    for period in permanence_period:
+        if (type(period[0]) is not int or type(period[1]) is not int):
+            return None
+        if (period[0] <= target_time <= period[1]):
+            counter += 1
+    
+    return counter
+    
