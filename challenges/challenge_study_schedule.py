f4db0cf270d7243abaf99bc21bfbb81b90860642
edison-ps <edison@osite.com.br>
2021-10-29 10:55:18 -0300

    Requisito 1 V1.01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,10 +5,10 @@ def study_schedule(permanence_period, target_time):
     study_counter = 0
 
     for period in permanence_period:
-        if (not isinstance(period[0], int) != 'int' or not isinstance(period[1], int) or
-            not isinstance(target_time, int)):
+        if (not isinstance(period[0], int) or not isinstance(period[1], int) or
+                not isinstance(target_time, int)):
             return None
-            
+
         if (target_time >= period[0] and target_time <= period[1]):
             study_counter += 1
 
