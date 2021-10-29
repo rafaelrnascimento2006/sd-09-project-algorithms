28403f0d9b361b39ca3c8edd81f39e5f90db7d43
lucasvr39 <lucasveras1994@gmail.com>
2021-10-29 15:31:44 -0300

    add req 3

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    mid_index = len(word) // 2
+    # inverted_word = word[::-1]
+    for i in range(mid_index):
+        if word[i] != word[-(i + 1)]:
+            return False
+
+    return True
