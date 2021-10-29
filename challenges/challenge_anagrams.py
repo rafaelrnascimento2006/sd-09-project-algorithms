1086bcf1c2708b93f74f5aa35bcb6446a85641b7
Vagner Pereira <sousa.vagner82@gmail.com>
2021-10-29 12:53:08 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    count = 0
+    for i in first_string:
+        for j in second_string:
+            if i == j:
+                count += 1
+                break
+    if count == len(first_string):
+        return True
+    else:
+        return False
