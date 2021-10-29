795a5d2ec239d98b66c42b60daaf410924fc6e54
edison-ps <edison@osite.com.br>
2021-10-29 10:44:40 -0300

    Requisito 1 V1.00

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """ Essa função retorna o número de estudantes
+        em um determinado horário target_time """
+
+    study_counter = 0
+
+    for period in permanence_period:
+        if (not isinstance(period[0], int) != 'int' or not isinstance(period[1], int) or
+            not isinstance(target_time, int)):
+            return None
+            
+        if (target_time >= period[0] and target_time <= period[1]):
+            study_counter += 1
+
+    return study_counter
