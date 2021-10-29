ce4e03efe2b7be4579713488ec5e7389c96dd88b
Vagner Pereira <sousa.vagner82@gmail.com>
2021-10-29 13:00:50 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,13 +1,11 @@
 def is_anagram(first_string, second_string):
-    if len(first_string) != len(second_string):
-        return False
     count = 0
     for i in first_string:
         for j in second_string:
             if i == j:
                 count += 1
                 break
-    if count == len(first_string):
+    if count == len(first_string) == len(second_string):
         return True
     else:
         return False
