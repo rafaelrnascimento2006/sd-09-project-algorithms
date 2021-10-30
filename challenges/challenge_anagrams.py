a9185b1ff998a5807aeefc0140c25b72b0cb324f
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-10-30 11:38:59 -0300

    fixing linter

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,5 @@
-#refazendo código do req3 em face da determinação de não poder pegar lib pronta
-#embora haja certa dúvida se o [::1] se encaixaria nisso
+# refazendo código do req3 em face da determinação de não poder pegar lib pronta
+# embora haja certa dúvida se o [::1] se encaixaria nisso
 
 def is_anagram(first_string, second_string):
     if not first_string:
@@ -8,25 +8,23 @@ def is_anagram(first_string, second_string):
     if not second_string:
         return False
 
-#como converter string em array em Python
-#https://www.delftstack.com/pt/howto/python/python-string-to-list/
+# como converter string em array em Python
+# https://www.delftstack.com/pt/howto/python/python-string-to-list/
     list_first_string = list(first_string)
     list_second_string = list(second_string)
 
-    quicksort(list_first_string, 0, len(list_first_string) -1)
-    quicksort(list_second_string, 0, len(list_second_string) -1)
-
-    result = False
+    quicksort(list_first_string, 0, len(list_first_string) - 1)
+    quicksort(list_second_string, 0, len(list_second_string) - 1)
 
     for i in range(len(list_first_string)):
         if list_first_string[i] == list_second_string[i]:
-            resp = True
+            are_anagrams = True
         else:
             return False
-    return resp
+    return are_anagrams
 
 
-#implementação conforme Course
+# implementação conforme Course
 def quicksort(array, low, high):
     # caso base: se já atingiu a menor porção (1)
     if len(array) == 1:
