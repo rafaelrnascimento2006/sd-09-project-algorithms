e0628c2e1ef67039c037f3cb0c7134b0f84dfde8
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-10-30 11:08:14 -0300

    try req 3.1

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -8,10 +8,10 @@ def is_anagram(first_string, second_string):
     reverse_first_string = first_string[::-1]
     reverse_second_string = second_string[::-1]
 
-    if second_string != reverse_first_string:
-        return False
+    if second_string == reverse_first_string:
+        return True
 
-    if first_string != reverse_second_string:
-        return False
+    if first_string == reverse_second_string:
+        return True
 
-    return True
+    return False
