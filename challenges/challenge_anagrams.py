b0478674bc7c2ad4945839c8caaae63831c3e7e7
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-10-30 10:51:09 -0300

    testing req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string:
+        return False
+
+    if not second_string:
+        return False
+
+    reverse_word = first_string[::-1];
+
+    if second_string != reverse_word:
+        return False
+
+    return True
