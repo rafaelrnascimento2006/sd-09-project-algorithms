e5aea41c98e3df58ce884e54b0121601243a87f9
marcusWittho <mcswitthoeft@gmail.com>
2021-10-30 19:11:33 -0300

    Resolução do requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        count = 0
+        for time in permanence_period:
+            if time[0] <= target_time and time[1] >= target_time:
+                count += 1
+
+        return count
+    except TypeError:
+        return None
