e8db710b260fafbb0f6ba14c3dda503a2c784113
brenoalv3s <brenoalv3ss@gmail.com>
2021-10-30 09:46:24 -0300

    01 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui """
+    try:
+        count = 0
+        for schedule in permanence_period:
+            if schedule[0] <= target_time and schedule[1] >= target_time:
+                count += 1
+        return count
+    except TypeError:
+        return None
