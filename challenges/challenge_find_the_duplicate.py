458e352a8bfddd9b97091df83f0062e36effc662
Julianafatsil <julianafatsil@gmail.com>
2021-10-30 12:32:42 -0300

    Challenge 04 - [Refactoring] tried to reduce the complexity of the algorithm

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
+def validate_length_or_empty(nums):
+    return (len(nums) < 2) or nums is None
+
+
+def validate_is_number_or_min_number(numero):
+    return type(numero) is not int or numero < 1
+
+
+def validate_number_duplicate(numero_duplicado):
+    if numero_duplicado == 0:
+        return False
+    numero_duplicado
+
+
 def find_duplicate(nums):
     """1º validações sem nullo e com menos de 2 valores
     2º ordenação com validações, se é numero, e se já existe
@@ -8,22 +22,18 @@ def find_duplicate(nums):
     existir, se já existir, guarda na variavel , continua fazendo para validar
     se não tem negativos ou string
     """
-
-    if (len(nums) < 2) or nums is None:
+    if validate_length_or_empty(nums):
         return False
     array_completo = []
     numero_duplicado = 0
-
     for numero in nums:
-        if type(numero) is not int or numero < 1:
+        if validate_is_number_or_min_number(numero):
             return False
         if numero not in array_completo:
             array_completo.append(numero)
         elif numero != numero_duplicado:
             numero_duplicado = numero
-    if numero_duplicado == 0:
-        return False
-    return numero_duplicado
+    return validate_number_duplicate(numero_duplicado)
 
 
 # print(find_duplicate([2, 5, 8, 4, 2, 1]))
