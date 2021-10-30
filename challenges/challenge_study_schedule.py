c2ee0cf2668caf3266b523d97c1eb7108868cd5d
MEianelli <filipelouzano@gmail.com>
2021-10-30 12:11:57 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,22 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time or target_time==0:
+    if not target_time or target_time == 0:
         return None
     counter = 0
     for period in permanence_period:
-        if (isinstance(period[0], int) 
-        and isinstance(period[1], int) 
-        and isinstance(target_time, int)):
+        if (isinstance(period[0], int)
+            and isinstance(period[1], int)
+                and isinstance(target_time, int)):
             if int(period[0]) <= int(target_time) <= int(period[1]):
                 counter += 1
         else:
             counter = None
     return counter
-
-
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-
-print(study_schedule(permanence_period,5))
-print(study_schedule(permanence_period,4))
-print(study_schedule(permanence_period,3))
-print(study_schedule(permanence_period,2))
-print(study_schedule(permanence_period,1))
