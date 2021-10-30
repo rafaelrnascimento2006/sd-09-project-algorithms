ebb71de2972c297d1125c6962e3dd62fa88048a2
brenoalv3s <brenoalv3ss@gmail.com>
2021-10-30 10:40:58 -0300

    02 - Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+
+    if len(word) < 1:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return False
