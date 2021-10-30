c0b2de4727219d926962cf9551b57536ac09665c
brenoalv3s <brenoalv3ss@gmail.com>
2021-10-30 13:52:41 -0300

    Anagramas (Algoritmo de ordenação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
+from collections import Counter
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return Counter(first_string) == Counter(second_string)
+
+
+# Referências:
+# https://docs.python.org/3/library/collections.html#collections.Counter
+# https://stackoverflow.com/questions/48217471/is-it-possible-to-check-for-anagram-without-using-sorted-or-dictionary-that-pe#:~:text=The%20pythonic%20way,s1)%20%3D%3D%20Counter(s2)
