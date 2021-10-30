40bd70cfab0693ed729ce7d8b3fb27d13546a0ee
Uilton <itoclick@gmail.com>
2021-10-30 11:50:49 -0300

    req4 ok

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,15 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+
+    if len(nums) < 2 or type(nums[0]) is str:
+        return False
+
+    if nums[0] < 0 or nums[1] < 0:
+        return False
+
+    nums.sort()
+
+    for index in range(len(nums)-1):
+        if nums[index + 1] == nums[index]:
+            return nums[index]
+
+    return False
--- testando.py
+++ testando.py
@@ -1,47 +1,20 @@
-"""
-niners = [40, 42, 9, 17, 27]
 
 
-def inversao(niners):
-    if len(niners) == 0:
-        return niners
-    print(niners)
-    resultado = inversao(niners[:len(niners)-1])
-    print(resultado)
-    print(niners)
-    return [niners[-1]] + resultado
+def find_duplicate(nums):
 
-
-a = print(inversao(niners))
-
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-
-
-def teste(permanence_period, target_time):
-    if target_time is None:
-        return None
-    for index, value in enumerate(permanence_period):
-        if None in value:
-            return None
-        acc = 0
-        if target_time in list(range(value[0], value[1]+1)):
-            acc += 1
-    return acc
-
-
-teste(permanence_period, 4)
- """
-
-
-def is_palindrome_iterative(word):
-    if word == "":
+    if len(nums) < 2 or type(nums[0]) is str:
         return False
-    x = ""
-    for value in word[::-1]:
-        x += value
-    return x == word
+
+    nums.sort()
+    x = False
+    for index in range(len(nums)-1):
+        if nums[index + 1] == nums[index]:
+            return nums[index]
+        x = False
+
+    return x
 
 
-wrd = "esse"
+num = [1, 2]
 
-print(is_palindrome_iterative(wrd))
+print(find_duplicate(num))
