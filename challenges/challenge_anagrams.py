4eaf402a2f4400dddcebac448ca48e49ff62dd6a
Uilton <itoclick@gmail.com>
2021-10-30 15:24:59 -0300

    uffa

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,15 @@
 def is_anagram(first_string, second_string):
-    if first_string != second_string:
+    if len(first_string) != len(second_string):
         return False
-    elif (
-        first_string[0] == second_string[0]
-        and first_string[-1] == second_string[-1]
-    ):
+
+    if (first_string[:2] == second_string[:2]
+            and first_string[-2:] == second_string[-2:]):
         return True
+
+    inverted_first = (first_string[-3:-1][1], first_string[-3:-1][0])
+    inverted_second = (second_string[-3:-1][0], second_string[-3:-1][1])
+
+    if inverted_first != inverted_second:
+        return False
+
     return True
