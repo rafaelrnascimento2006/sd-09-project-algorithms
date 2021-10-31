36346dded2767bb3cc9810040d52aef3729d3dd7
Douglas Ferreira <douglas.ferreira@mutual.club>
2021-10-31 13:58:35 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    
+    total = 0
+
+    for item in permanence_period:
+        if item[0] is None or item[1] is None:
+            return None
+        if item[0] <= target_time <= item[1]:
+            total += 1
+
+    return total
