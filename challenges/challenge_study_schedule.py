ab971839349004ee4512eac52a91cf5e1c2dcd2a
LucioOSilva <lucioosilva00@gmail.com>
2021-10-31 10:41:43 -0300

    Fix lint issues

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
     try:
-        for i,j in permanence_period:
+        for i, j in permanence_period:
             if i <= target_time <= j:
                 counter += 1
         return counter
