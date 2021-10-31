c2eb0cb3f03f6b429d2a5462b6a3fa3c9c49a1d0
cleber-teixeira <cleberlt@gmail.com>
2021-10-31 00:37:59 -0300

    Req 03 - Anagramas (Algoritmo de ordenação). :white_check_mark:

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if first_string == '' or second_string == '' or (
+        len(first_string) != len(second_string)
+    ):
+        return False
+    else:
+        for i in range(len(first_string)):
+            if len(first_string) > 0:
+                second_string = second_string.replace(first_string[0], '')
+                first_string = first_string.replace(first_string[0], '')
+            if len(first_string) != len(second_string):
+                return False
+        return True
