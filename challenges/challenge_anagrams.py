48543fda69fd4649a39417142e35ce91adcfb32b
marcusWittho <mcswitthoeft@gmail.com>
2021-10-30 22:00:22 -0300

    Requsito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    for i in first_string:
+        if first_string.count(i) != second_string.count(i):
+            return False
+
+    return True
