6832fe427d8d4bdeb1cf353ca6530c72552962ec
jhon <jhonatan.arguello@gmail.com>
2021-10-31 16:38:15 -0300

    Finalizaçao dos testes 2 e 3 do requisito 1.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_total = 0
+    if type(target_time) != int:
+        return None
+
+    for period in permanence_period:
+        entrance = period[0]
+        exit = period[1]
+
+        if type(permanence_period) != tuple:
+            return None
+
+        if entrance <= target_time <= exit:
+            students_total += 1
+
+    return students_total
