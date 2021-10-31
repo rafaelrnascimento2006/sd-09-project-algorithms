47c978c29cd8312b669e70cd163aad97355aad9d
LucioOSilva <lucioosilva00@gmail.com>
2021-10-31 10:20:40 -0300

    Added requirement01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for i,j in permanence_period:
+            if i <= target_time <= j:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
