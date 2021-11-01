fd7286f9d67f73dbdb7e4d58b3cb794bfe73ba5f
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-11-01 15:27:34 -0300

    Sera que vai?

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    if _is_anagram(first_string) == _is_anagram(second_string):
+        return True
+    else:
+        return False
+
+
+def _is_anagram(string):
+    condition = True
+    string = list(string)
+    counter = 0
+    while condition:
+        condition = False
+
+        for index in range(len(string) - counter - 1):
+            if string[index] > string[index+1]:
+                string[index], string[index+1] = string[index+1], string[index]
+                condition = True
+
+        counter += 1
+
+    return "".join(string)
