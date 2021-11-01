630065d5a7675ccdcdd2504575ca0165545e26af
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-11-01 10:56:00 -0300

    Req. 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    if word[low_index] == word[high_index]:
+        if low_index+1 == high_index or low_index+2 == high_index:
+            return True
+        return is_palindrome_recursive(word, low_index+1, high_index-1)
+    else:
+        return False
