a69d492b53d3af1516c0bf2f9df0c49743ebfcae
ViniciusChagas <viniciushrcs@gmail.com>
2021-11-01 19:46:23 -0300

    Req 01, corrigido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     counter = 0
 
     for entry, exit in permanence_period:
-        if not (entry or exit) or target_time is None:
+        if (not entry or not exit) or target_time is None:
             return None
         else:
             if entry <= target_time <= exit:
