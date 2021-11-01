14830d661afab5cfdc26e6840336082d614e878b
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-11-01 12:34:16 -0300

    Feat: ta indo... Vamos que vamos...

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,27 +1,13 @@
-def is_palindrome_recursive(word, low_index, high_index): #inicio = low_index fim = high_index
+def is_palindrome_recursive(word, low_index, high_index):
+    # inicio = low_index fim = high_index
     # print(low_index, high_index)
     if word == '':
         return False
     if word[low_index] != word[high_index]:
-      return False
+        return False
     else:
-      if len(word)-1 == low_index and high_index == 0:
-          return True
-    return is_palindrome_recursive(word,low_index + 1, high_index -1)
+        if len(word)-1 == low_index and high_index == 0:
+            return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
-
-print(is_palindrome_recursive('arara', 0, 4))
-
-# def eh_palindromo(palavra):
-#     palavra = palavra.lower()
-#     inicio = 0
-#     fim = len(palavra)-1
-#     for i in range(len(palavra)//2):
-#         if palavra[inicio] != palavra[fim]:
-#             return False
-#         inicio = inicio + 1
-#         fim = fim - 1
-#     return True
-# print(eh_palindromo('Osso'))
-
-
+# print(is_palindrome_recursive('arara', 0, 4))
