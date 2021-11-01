e4ef91cbdd16471721f04fa788fb752e49e27326
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-11-01 18:55:46 -0300

    Req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_list = list(first_string)
+    second_list = list(second_string)
+
+    if len(first_list) != len(second_list):
+        return False
+
+    for letter in first_list:
+        if letter not in second_list:
+            return False
+        else:
+            return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,7 +2,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if not word or word == "" or word[low_index] != word[high_index]:
         return False
 
-    if low_index == high_index:
+    if low_index == high_index or high_index - low_index < 1:
         return True
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
