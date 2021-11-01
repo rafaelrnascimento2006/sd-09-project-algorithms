bf3a3c7d8bc9154b4f913dccb3ebba37901d0fb1
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-11-01 10:20:57 -0300

    Req. 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+
+    for entry_period, exit_period in permanence_period:
+        if entry_period and exit_period:
+            if target_time >= entry_period and target_time <= exit_period:
+                counter += 1
+        else:
+            return None
+    return counter
