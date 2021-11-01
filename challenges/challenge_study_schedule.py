40fb148af397929f6a42025a99a913e63c835323
galaraz <jvictor.si.2013@gmail.com>
2021-10-31 23:49:33 -0300

    quase la

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-# Nos arrays temos 6 estudantes
 
-# estudante             1       2       3       4       5       6
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+    better_occurrence  = 0
 
-target_time = 5  # saída: 3, pois a quarta, a quinta e a sexta pessoa estudante ainda estavam estudando nesse horário.
-target_time = 4  # saída: 3, pois a quinta e a sexta pessoa estudante começaram a estudar nesse horário e a quarta ainda estava estudando.
-target_time = 3  # saída: 2, pois a terceira e a quarta pessoa estudante ainda estavam estudando nesse horário.
-target_time = 2  # saída: 4, pois a primeira, a segunda, a terceira e a quarta pessoa estudante estavam estudando nesse horário.
-target_time = 1  # saída: 2, pois a segunda e a quarta pessoa estudante estavam estudando nesse horário.
+    for period in permanence_period:
+        try:
+            if period[0] <= target_time <= period[1]:
+                better_occurrence  += 1
+        except TypeError:
+            return None
 
-""" Para esse exemplo, depois de rodar a função para todos esses `target_times`,
-julgamos que o melhor horário é o `2`, pois esse retornou `4`, já que 4 estudantes estavam presentes nesse horário!
- """
+    return better_occurrence
