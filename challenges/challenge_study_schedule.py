3ff0aab0cd39e59e7746ded0c4199d9b88391819
Leticia Lima <letylima96@gmail.com>
2021-11-02 20:53:56 -0300

    primeiro requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_qtty = 0
+    if not target_time:
+        return None
+    for (login, logout) in permanence_period:
+        if login and logout:
+            if login <= target_time <= logout:
+                students_qtty += 1
+        else:
+            return None
+    return students_qtty
