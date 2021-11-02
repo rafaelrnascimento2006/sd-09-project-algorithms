0aadeaab9b73f8e8378115a3eeb20791aa2009fb
muriloPereira006 <muriloedu006@gmail.com>
2021-11-02 01:28:58 -0300

    first commit, desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    people_studing = 0
+
+    if target_time:
+        for period in permanence_period:
+            #print(period)
+
+            if not period[0] or not period[1]:
+                return None
+            
+            if period[0] <= target_time <= period[1]:
+                people_studing += 1
+        
+        return people_studing
