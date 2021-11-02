9ca3657cfc61cb6f8f806123234c3e99676bc39f
muriloPereira006 <muriloedu006@gmail.com>
2021-11-02 03:03:01 -0300

    .

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,12 +4,11 @@ def study_schedule(permanence_period, target_time):
 
     if target_time:
         for period in permanence_period:
-            #print(period)
 
             if not period[0] or not period[1]:
                 return None
-            
+
             if period[0] <= target_time <= period[1]:
                 people_studing += 1
-        
+
         return people_studing
