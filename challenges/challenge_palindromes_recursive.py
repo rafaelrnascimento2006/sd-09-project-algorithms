5eefec9210cfa73954d441f4ba06a0004bac9e4e
danielfasanaro <danielfasanaro@gmail.com>
2021-11-02 15:56:52 -0300

    feat: 2nd Requirement

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    elif high_index == 0:
+        return True
+    reverse_word = word[::-1]
+    if word == reverse_word:
+        return is_palindrome_recursive(word, 0, high_index - 1)
+    else:
+        return False
