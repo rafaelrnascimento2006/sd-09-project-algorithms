0364cb7e820b628882d6f4bf17aa1b9272c97866
galaraz <jvictor.si.2013@gmail.com>
2021-11-01 23:02:37 -0300

    fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
 def study_schedule(permanence_period, target_time):
 
-    better_occurrence  = 0
+    better_occurrence = 0
 
     for period in permanence_period:
         try:
             if period[0] <= target_time <= period[1]:
-                better_occurrence  += 1
+                better_occurrence += 1
         except TypeError:
             return None
 
