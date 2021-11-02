dfba55c449f69fadfebb3331eb67e9518626e99f
galaraz <jvictor.si.2013@gmail.com>
2021-11-02 08:04:17 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+
+
+    if len(first_string) != len(second_string):
+
+        return False
+    
+    splitWord =[]
+
+    for letter in first_string:
+        splitWord.append(letter)
+
+
+
+    if letter in second_string:
+            return True
+    else:
+            return False   
+
