29ea2ec879a9d2d1908d1db9b3710732fb0b3707
muriloPereira006 <muriloedu006@gmail.com>
2021-11-02 02:58:22 -0300

    2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -6,7 +6,7 @@ def is_palindrome_recursive(word, low_index, high_index):
 
     if low_index >= high_index:
         return True
-    elif word[low_index] == word[high_index]:
+    if word[low_index] != word[high_index]:
+        return False
+    else:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-
-    return False
