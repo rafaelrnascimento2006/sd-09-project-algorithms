755c01b6692421edf4c02d1ef21e3f833e607db6
matheusjkl1 <matheus.unilog@gmail.com>
2021-11-02 08:58:16 -0300

    req 1, req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word == "":
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if high_index - low_index == 1 or high_index - low_index == 0:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+    for period in permanence_period:
+        entry, leave = period
+        if entry is None or leave is None:
+            return None
+        if target_time >= entry and target_time <= leave:
+            counter += 1
+    return counter
