64ef166460de3b8a6a2c21607c16c46551258f8a
muriloPereira006 <muriloedu006@gmail.com>
2021-11-02 20:14:34 -0300

    trapassa

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+
+    if len(first_string) == len(second_string):
+        first = first_string.split('')
+        second = second_string.split('')
+
+        if first.sort() == second.sort():
+            return True
+
+    return False
