6c5539629394aa99aac050a06086fddcc455482b
Nathália Veneziano <nathaliaveneziano@icloud.com>
2021-11-02 18:08:26 -0300

    Requisito 05

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 # Requisito 05
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    # [::-1] -> inverte a palavra
+    if word != word[::-1]:
+        return False
+
+    return True
