9167215a897e24de81b6588b05783ea2cbb342c1
Nathália Veneziano <nathaliaveneziano@icloud.com>
2021-11-02 20:59:48 -0300

    Requisito 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 # Requisito 04
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) < 2:
+        return False
+
+    sorted_nums = sorted(nums)
+
+    for index in range(len(sorted_nums) - 1):
+        if type(sorted_nums[index]) == str or sorted_nums[index] < 1:
+            return False
+
+        if sorted_nums[index] == sorted_nums[index + 1]:
+            return sorted_nums[index]
+
+    return False
