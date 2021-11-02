7167eecd9df0b7cc3705364d23c7e62acf9e1390
Renato Feitosa <pythaqua@gmail.com>
2021-11-02 20:05:54 -0300

    chore: Improve 1st req. algorithm space complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Returns amount of items in permanence_period
-    that contains target_time in its interval"""
+    """ Returns the amount of items in permanence_period that contains
+    target_time in its interval or None if any of them has an invalid value"""
+    count = 0
     try:
-        studying_periods = [i for i in permanence_period
-                            if i[0] <= target_time <= i[1]]
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                count += 1
     except TypeError:
         return None
-    return len(studying_periods)
+    return count
