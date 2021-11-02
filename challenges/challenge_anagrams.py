0e07dec95337b25000ec47f21736fc2a98bb17d1
RodrigoNeoEx <draja.acc@gmail.com>
2021-11-02 10:25:05 -0300

    upd: def is_anagram, counter can check all requisites

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,8 +4,7 @@ from collections import Counter
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
     # https://codefather.tech/blog/anagrams-python/
-    if len(first_string) != len(second_string):
-        return False
+
     if(Counter(first_string) == Counter(second_string)):
         return True
     else:
