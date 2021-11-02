63a88c6d8bc3a38df7e90a1299e4cd306531c950
Renato Feitosa <pythaqua@gmail.com>
2021-11-02 19:50:39 -0300

    feat: Add 1st requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """ Returns amount of items in permanence_period
+    that contains target_time in its interval"""
+    try:
+        studying_periods = [i for i in permanence_period
+                           if i[0] <= target_time <= i[1]]
+    except TypeError:
+        return None
+    return len(studying_periods)
