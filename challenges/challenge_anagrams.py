80ad442035278534d538e229af0c5a740ca7fe8c
Tiago Paz <tiagopaz.dev@gmail.com>
2021-11-02 13:28:16 -0300

    req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,28 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_array = []
+    second_array = []
+
+    for letter in first_string:
+        first_array.append(letter)
+    for letter in second_string:
+        second_array.append(letter)
+
+    return count_letters(first_array, second_array)
+
+
+def count_letters(first_array, second_array):
+    index = 0
+
+    while(index < len(first_array)):
+        for letter in second_array:
+            if(first_array[index] == letter):
+                second_array.remove(letter)
+        index += 1
+
+    if (len(second_array) == 0):
+        return True
+    else:
+        return False
