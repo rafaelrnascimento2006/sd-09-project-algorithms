46e7f613cd659d74c05c46ac90fe5058b49c94ca
Renato Feitosa <pythaqua@gmail.com>
2021-11-02 19:52:29 -0300

    fix: Solve lint issue

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,7 @@ def study_schedule(permanence_period, target_time):
     that contains target_time in its interval"""
     try:
         studying_periods = [i for i in permanence_period
-                           if i[0] <= target_time <= i[1]]
+                            if i[0] <= target_time <= i[1]]
     except TypeError:
         return None
     return len(studying_periods)
