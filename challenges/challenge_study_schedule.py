79b40910f8c6a0a6858a843953214eb71c0b5af8
danielfasanaro <danielfasanaro@gmail.com>
2021-11-02 15:33:45 -0300

    feat: 1st Requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    result = 0
+    for permanence in permanence_period:
+        if type(permanence[0]) is not int or type(permanence[1]) is not int:
+            return None
+        elif target_time is None:
+            return None
+        elif permanence[0] <= target_time <= permanence[1]:
+            result += 1
+    return result
