f3468b9c2a3df0055d89b8d6fdd6881d5b3b6ee4
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-11-02 23:01:17 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui! """
+    if low_index < len(word) / 2:
+        if not word:
+            return False
+        if word[low_index] != word[high_index]:
+            return False
+        else:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
+
+
+# https://academiahopper.com.br/recursao-python/
