283977ce72711b29eeb889e2e05a6368c9fb2fac
Renato Feitosa <pythaqua@gmail.com>
2021-11-03 01:01:33 -0300

    chore: !!incomplete 3rd and 4th reqs

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    list_size = len(nums)
+    is_valid_size = list_size > 1
+
+    if not is_valid_size:
+        return is_valid_size
+    
+    for i in nums:
+        if type(i) != int or i < 0:
+            return False
+        
+    return 0
+
