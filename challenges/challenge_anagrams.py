1be4eac32aa8b50caf99c5b60e2dd454c824b2f7
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-11-03 16:42:39 -0300

    Requisitos 2 e 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (
+        not (first_string and second_string)
+        or (len(first_string) != len(second_string))
+    ):
+        return False
+    for n in range(len(first_string)):
+        if not first_string.count(second_string[n]):
+            return False
+        if not (
+            second_string.count(first_string[n]) ==
+            first_string.count(second_string[n])
+        ):
+            return False
+    return True
