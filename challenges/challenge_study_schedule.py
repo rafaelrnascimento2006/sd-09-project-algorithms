49b03811f7a880b40f4079b66bb06dfdf7ef42c6
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-11-02 21:43:55 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui! """
+    counter = 0
+    if type(target_time) != int:
+        return None
+    for (login, logof) in permanence_period:
+        if type(login) != int or type(logof) != int:
+            return None
+        if login <= target_time <= logof:
+            counter += 1
+    return counter
