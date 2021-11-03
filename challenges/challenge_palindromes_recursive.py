e1dce72a8a412e5befd022c544620bf42d6fa30e
Emanoel Leão <Emanoel Leo>
2021-11-02 23:58:35 -0300

    Req. 02 - Fix

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word is None:
+    print(word, type(word))
+    if not word:
         return False
     if word[low_index] != word[high_index]:
         return False
     if low_index >= high_index:
         return True
-    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return is_palindrome_recursive(low_index + 1, high_index - 1)
+
+
+print(is_palindrome_recursive('', 0, 2))
