338760cf2f4c5cafa3ca2e2878de6200788da592
Rafael Pufal <rafael.pufal23@gmail.com>
2021-11-03 09:43:00 -0300

    Req 1.3

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,3 +3,6 @@ def study_schedule(permanence_period, target_time):
     # counter = 0
     if type(target_time) != int:
         return None
+    for period in permanence_period:
+        if (type(period[0]) is not int or type(period[1]) is not int):
+            return None
