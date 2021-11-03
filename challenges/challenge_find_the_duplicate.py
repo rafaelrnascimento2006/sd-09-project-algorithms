19e854718e32383f633cab0c8dced9c41b5337bb
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-11-03 17:00:17 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums or len(nums) < 1:
+        return False
+    for num in nums:
+        if num < 0 or type(num) != int:
+            return False
+        if nums.count(num) > 1:
+            return num
+    return False
---
+++ challenges/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+frog = [8,2,3,3,2,3]
+print(frog.count(3))
