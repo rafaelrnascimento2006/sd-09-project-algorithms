dfcc05d99600b7df237904e154720dc762efdc3f
Emanoel Leão <Emanoel Leo>
2021-11-03 00:53:35 -0300

    Req. 02 - Fix and Req. 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,30 @@
+def quicksort(array, low, high):
+    if len(array) == 1:
+        return array
+    if low < high:
+        partition_index = partition(array, low, high)
+        quicksort(array, low, partition_index - 1)
+        quicksort(array, partition_index + 1, high)
+
+
+def partition(array, low, high):
+    i = low - 1
+    pivot = array[high]
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i + 1
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_sorted = list(first_string)
+    second_sorted = list(second_string)
+    quicksort(first_sorted, 0, len(first_sorted) - 1)
+    quicksort(second_sorted, 0, len(second_sorted) - 1)
+    if first_sorted == second_sorted:
+        return True
+    else:
+        return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,4 +5,4 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if low_index >= high_index:
         return True
-    return is_palindrome_recursive(low_index + 1, high_index - 1)
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
