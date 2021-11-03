94b602f8197cbb1519b6b111e6cb4b6bdaeab2f0
Nathália Veneziano <nathaliaveneziano@icloud.com>
2021-11-02 21:35:45 -0300

    Requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 # Requisito 01
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time or len(permanence_period) == 0:
+        return None
+
+    count = 0
+
+    for entry_period, exit_period in permanence_period:
+        if entry_period and exit_period:
+            if entry_period <= target_time <= exit_period:
+                count += 1
+        else:
+            return None
+
+    return count
