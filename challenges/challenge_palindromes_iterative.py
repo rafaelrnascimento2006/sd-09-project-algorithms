29946dc2ff8f003e0d8d09088129ae3ef31bca0e
Renato Feitosa <pythaqua@gmail.com>
2021-11-03 13:38:48 -0300

    chore: Improve 5th req.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
-    return False
+    if not word:
+        return False
+
+    beginning, end = 0, len(word) - 1
+
+    while end - beginning >= 1:
+        if word[beginning] != word[end]:
+            return False
+        beginning += 1
+        end -= 1
+
+    return True
