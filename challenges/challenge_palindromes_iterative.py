99d9f68d7a520a81913a461aa4c2bbd430db5cc6
Mario Pardo <mariio.pardojr@gmail.com>
2021-11-02 23:38:37 -0300

    Req. 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,14 @@
+def get_reverse_word(word):
+    reverse_word_letter_list = []
+    for letter in word:
+        reverse_word_letter_list.insert(0, letter)
+    return ''.join(reverse_word_letter_list)
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    elif word == get_reverse_word(word):
+        return True
+    else:
+        return False
