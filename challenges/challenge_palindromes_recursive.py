7d19586a80d0026a8228a15e8d2d8acb33e6ddaf
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-11-03 00:52:38 -0300

    req 2.3 fix

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
+    if not word:
+        return False
     if low_index < len(word) / 2:
-        if not word:
-            return False
         if word[low_index] != word[high_index]:
             return False
         else:
