b8e9861ad25c6bfdd5ae47269c5b4e4040e32bf7
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-11-03 10:28:04 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui """
+    if not target_time:
+        return None
+    counter = 0
+    for (start, finish) in permanence_period:
+        if not(start or finish):
+            return None
+        if start <= target_time <= finish:
+            counter += 1
+    return counter
